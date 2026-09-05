6199c3726918d29795eaed3bdef5a549b1d182c6
Oli Vickery <olivickery@Olis-MBP.modem>
2026-09-05 20:21:18 +1000

    Move TackleVault.svg to public and render cleanly in VaultIcon component

--- src/app/components/VaultIcon.tsx
+++ src/app/components/VaultIcon.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 
 export function VaultIcon({ className = "w-5 h-5" }: { className?: string }) {
   return (
-    <svg 
-      xmlns="http://www.w3.org/2000/svg" 
-      viewBox="0 0 512 512" /* Match your Illustrator Artboard size */
-      fill="currentColor"
-      className={className}
-    >
-      {src/app/TackleVault.svg}
-    </svg>
+    <img 
+      src="/TackleVault.svg" 
+      alt="Tackle Vault Logo" 
+      className={className} 
+    />
   );
 }
