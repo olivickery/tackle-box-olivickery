0d95b2d8e6ac1e81abc8266684e807ad916bb5d6
Oli Vickery <olivickery@Olis-MBP.modem>
2026-09-05 20:00:45 +1000

    Add custom Illustrator vault icon and mobile home screen app icons

---
+++ src/app/components/VaultIcon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function VaultIcon({ className = "w-5 h-5" }: { className?: string }) {
+  return (
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      viewBox="0 0 512 512" /* Match your Illustrator Artboard size */
+      fill="currentColor"
+      className={className}
+    >
+      {/* /Users/olivickery/Desktop/tackle-box-olivickery/src/app/TackleVault.svg */}
+    </svg>
+  );
+}
